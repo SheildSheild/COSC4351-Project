334177099bb9203cd28ade1528c461d97cb90998
SheildSheild <sctheswordbearer@gmail.com>
2024-06-19 15:04:22 -0500

    fixed link variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
