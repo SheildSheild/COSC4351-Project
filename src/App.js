c7a0576940663438d3421c06fd3317f58f6c8c23
SheildSheild <sctheswordbearer@gmail.com>
2024-06-19 15:12:22 -0500

    babel install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-const { TextDecoder, TextEncoder } = require('util');
-global.TextDecoder = TextDecoder;
-global.TextEncoder = TextEncoder;
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
-
+const { TextDecoder, TextEncoder } = require('util');
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
 
 function App() {
   return (
