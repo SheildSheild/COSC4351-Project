570abe6a349b3bc43ebd874ec07fc4c41184ee7e
SheildSheild <sctheswordbearer@gmail.com>
2024-06-19 15:08:59 -0500

    text decoder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+const { TextDecoder, TextEncoder } = require('util');
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+
+
 
 function App() {
   return (
