24c9519792f124788ca51c07a58bf8d8785409ff
Tommy To <Tommy@t-t-mac-book.local>
2024-06-27 18:14:00 -0500

    update adminHistory.js

--- src/components/adminHistory/adminHistory.js
+++ src/components/adminHistory/adminHistory.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './adminHistory.css';
+import './adminHistory.css'; // Ensure you have appropriate CSS styles
+import fakeEvents from '../mockData/fake_event.json'; // Adjust the path as per your file structure
 
-const adminHistory = () => {
+const AdminHistory = () => {
   return (
-    <div className="user-event-page">
-      <h2>Admin History Page</h2>
+    <div className="admin-history-container">
+      <h2 className='volunteer-history-heading'>Volunteer History</h2>
+      {fakeEvents.map((event) => (
+        <div key={event.id} className="event-box">
+          <div className="event-info">
+            <div><strong>Event Name:</strong> {event.name}</div>
+            <div><strong>Description:</strong> {event.description}</div>
+            <div><strong>Location:</strong> {event.location}</div>
+            <div><strong>Date:</strong> {event.date}</div>
+            <div><strong>Urgency:</strong> {event.urgency.name}</div>
+          </div>
+          <hr className="line" />
+        </div>
+      ))}
     </div>
   );
 };
 
-export default adminHistory;
+export default AdminHistory;
