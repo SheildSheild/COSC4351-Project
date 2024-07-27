b131e80b7ecb09b0f66fdf2222fc40dd9872200b
mahir <mahiralam3400@gmail.com>
2024-07-26 21:24:13 -0500

    t

--- server/mongoConnect.js
+++ server/mongoConnect.js
@@ -1,12 +1,27 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://keeansmith1:keens@cosc4353.onpjljk.mongodb.net/?retryWrites=true&w=majority&appName=COSC4353";
 
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://keeansmith1:<password>@cosc4353.onpjljk.mongodb.net/?retryWrites=true&w=majority&appName=COSC4353";
+const client = new MongoClient(uri, {
+  serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsInsecure: true,
+});
+
 let conn;
-try {
-  conn = await client.connect();
-} catch(e) {
-  console.error(e);
+let db;
+
+async function connectToDatabase() {
+  try {
+    conn = await client.connect();
+    db = conn.db("COSC4353");
+    console.log("Connected to database!");
+  } catch (e) {
+    console.error("Failed to connect to database", e);
+  }
 }
-let db = conn.db("COSC4353");
+
+await connectToDatabase();
+
 export default db;
